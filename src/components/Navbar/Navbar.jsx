be1760dd62a314ade78f404c346d2035d50ae269
Ramees C <rameesc985@gmail.com>
2025-01-24 14:11:46 +0530

    navbar links added in small screen and menu and close button added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/logo/Dhanwis_logo1.png"
 
 import Darkmode from "../Darkmode/Darkmode";
+
+import { IoIosMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+
 
 
 const MenuLinks = [
@@ -38,6 +42,10 @@ const MenuLinks = [
 ];
 
 function Navbar() {
+
+
+  const [isOpen, setIsOpen] = useState(true)
+
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       <div className="py-4">
@@ -52,14 +60,25 @@ function Navbar() {
             </a> */}
             <a href=""><img src={logo} alt="" width={'120px'} /></a>
 
+              <div onClick={() => setIsOpen(!isOpen )} className="text-4xl text-primary absolute right-6 top-6 cursor-pointer md:hidden">
+                {
+                  isOpen ?
+                  <IoMdClose />
+                  :
+                  <IoIosMenu />
+                }
+                
+                
+              </div>
+
             {/* Menu Items */}
-            <div className="hidden lg:block">
-              <ul className="flex items-center gap-4">
+            <div>
+              <ul className={`md:flex pt-10 sm:pt-0 md:items-center md:pb-0 pb-12 absolute md:static bg-white dark:bg-gray-900 dark:text-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9  ${isOpen ? 'top-20 opacity-100' : 'top-[-490px]'} md:opacity-100`}>
                 {MenuLinks.map((menu, index) => (
                   <li key={index}>
                     <a
                       href=""
-                      className="inline-block px-4 font-semibold text-grey-500 hover:text-primary dark:hover-text-white"
+                      className="inline-block px-4 py-2 sm:py-0 font-semibold text-grey-500 hover:text-primary dark:hover-text-white"
                     >
                       {menu.name}
                     </a>
