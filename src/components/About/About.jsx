c13215e92f52a3bf74b62b23587fcadd99f0d5eb
Ramees C <rameesc985@gmail.com>
2025-02-16 12:49:20 +0530

    services component description changed and home page about section content changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,10 +7,9 @@ import Button from "../Shared/Button";
 import { Link, useNavigate } from "react-router-dom";
 
 function About() {
-
   const handleClick = () => {
-    window.scrollTo(0,0)
-  }
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div>
@@ -31,9 +30,10 @@ function About() {
           <div className="max-w-[1320px] lg:py-[80] py-5 flex xl:flex-row flex-col justify-center items-center mx-auto">
             {/* Content section */}
             <div
-            data-aos="fade-right"
-            data-aos-duration="500"
-            className="basis-[45%]">
+              data-aos="fade-right"
+              data-aos-duration="500"
+              className="basis-[45%]"
+            >
               <img
                 src={about_img}
                 className="w-full rounded-xl mb-10 xl:mb-0"
@@ -41,26 +41,25 @@ function About() {
               />
             </div>
 
-            <div 
-            data-aos="fade-left"
-            data-aos-duration="500"
-            className="basis-[45%] ml-0 xl:ml-10">
+            <div
+              data-aos="fade-left"
+              data-aos-duration="500"
+              className="basis-[45%] ml-0 xl:ml-10"
+            >
               <h1 className="uppercase font-semibold text-2xl text-primary mb-3">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Facilis, impedit.
+                Empowering Futures Through Software Development and Digital
+                Marketing Excellence.
               </h1>
               <p className="mb-5 text-black/85 dark:text-white text-justify [word-spacing:0.1rem]	">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit
-                laudantium ullam vero! Fugit obcaecati praesentium error facere
-                omnis ipsa eveniet laudantium perferendis. Alias, quam maxime,
-                quasi qui debitis voluptatibus dignissimos quas fugit eligendi
-                minima natus molestias porro aut error voluptate exercitationem
-                dolor neque rem, nulla dolore placeat nobis? Aliquam delectus
-                eveniet incidunt asperiores optio dignissimos, debitis maxime,
-                blanditiis explicabo nobis velit in quia quis rerum fugiat
-                aperiam consectetur corrupti similique distinctio molestias.
-                Rerum adipisci eos harum assumenda officia iusto est labore vel
-                in
+                Our academy is a hub for mastering Full Stack Web Development,
+                Mobile App Development, and Digital Marketing. We provide
+                industry-driven training programs tailored to meet the demands
+                of the ever-evolving tech world. With hands-on projects,
+                real-time case studies, and expert guidance, we ensure you gain
+                practical skills and in-depth knowledge. Whether you're starting
+                your journey or upgrading your expertise, our mission is to
+                empower you with the tools to succeed in software development
+                and digital marketing. Let’s build your future together!
               </p>
 
               <div className="flex flex-col justify-center items-start gap-4">
@@ -80,17 +79,18 @@ function About() {
             </div>
           </div>
           <div
-          data-aos="fade-up"
-          data-aos-offset="0"
-          data-aos-duration="500"
-          data-aos-delay="300"
-          className="flex justify-center mt-5">
-            <Link to='/about' onClick={handleClick}>
-            <Button
-              text="View More"
-              bgColor="bg-primary"
-              textColor="text-black" 
-            />
+            data-aos="fade-up"
+            data-aos-offset="0"
+            data-aos-duration="500"
+            data-aos-delay="300"
+            className="flex justify-center mt-5"
+          >
+            <Link to="/about" onClick={handleClick}>
+              <Button
+                text="View More"
+                bgColor="bg-primary"
+                textColor="text-black"
+              />
             </Link>
           </div>
         </div>
