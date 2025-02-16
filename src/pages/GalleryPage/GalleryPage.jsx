4bd12a9884be7715a767f7cb0fa1f27e4312a135
Ramees C <rameesc985@gmail.com>
2025-02-16 12:33:17 +0530

    remove loading gallery page and light gallery module removed

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -7,49 +7,30 @@ import img3 from "../../assets/images/gallery/g3.jpg";
 import img4 from "../../assets/images/gallery/g4.jpg";
 import img5 from "../../assets/images/gallery/g5.jpg";
 
-import spinner from "../../assets/images/loading_spinner/loading_spinner.gif";
 import WhatsappIcon from "../../components/WhatsappIcon/WhatsappIcon";
 
-import LightGallery from "lightgallery/react";
-
-// import styles light gallery
-import "lightgallery/css/lightgallery.css";
-import "lightgallery/css/lg-zoom.css";
-import "lightgallery/css/lg-thumbnail.css";
 import CallIcon from "../../components/CallIcon/CallIcon";
 
 function GalleryPage() {
-  const [isLoading, setIsLoading] = useState(false);
-
   const Images = [img1, img2, img3, img4, img5];
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(true);
-    }, 4000);
-  }, []);
-
-  return !isLoading ? (
-    <div className="loading_spinner">
-      <img src={spinner} alt="" />
-    </div>
-  ) : (
+  return (
     <div>
       <div className="container">
         {/* page header */}
         <PageHeader title="Gallery" />
 
         <div className="columns-1 sm:columns-2 lg:columns-3 py-10 md:py-20 gap-4">
-          <LightGallery speed={500} plugins={[]} mode="lg-fade" download={false}>
+          <div speed={500} plugins={[]} mode="lg-fade" download={false}>
             {Images.map((image, index) => (
               <img
                 key={index}
                 src={image}
                 alt="image"
-                className="w-full object-cover rounded-lg cursor-pointer h-full mb-4 break-inside-avoid"
+                className="w-full object-cover rounded-lg cursor-pointer h-full mb-4 break-inside-avoid transition duration-300 ease-in-out hover:scale-110"
               />
             ))}
-          </LightGallery>
+          </div>
         </div>
       </div>
 
